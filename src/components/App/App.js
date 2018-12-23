b2083f5540f96d9eaa958a9d8474c8f1adbb7297
Ilya Makov <makiliv72@gmail.com>
2018-12-23 19:28:17 +0300

    commit before update from remote repo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,20 +6,26 @@ import './app.css'
 
  class App extends Component {
    state = {
-    messages: ['Item']
+    messages: []
    }
 
    messages = this.state.messages;
 
-  inputHandler = (e) => {
-    if(e.keyCode === 13 && e.target.value) {
-      this.messages.push(e.target.value)
-      // this.setState({
-      //   messages: e.target.value
-      // });
+   inputHandler = (e) => {
+    const { messages } = this.state;
+    if(e.key === 'Enter' && e.target.value) {
+      // this.messages.push(e.target.value);
+      this.setState({
+        messages: [...messages, e.target.value]
+      });
       e.target.value = '';
       console.log(this.state)
     }
+  }
+
+
+   func = (e) => {
+    this.inputHandler(e);
   }
 
    render() {
@@ -27,7 +33,7 @@ import './app.css'
        <div className="App chat">
           <ChatList messages={this.state.messages} / >
           <ChatInput 
-            onEnter={this.inputHandler}
+            onEnter={this.func}
 
           / >
         </div>
--- src/components/chat-input/chat-input.js
+++ src/components/chat-input/chat-input.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import './chat-input.css'
-
+import './chat-input.css';
 
 class ChatInput extends Component {
-
-  // constructor(props) {
-  //   super(props)
-  // }
-
-  // inputHandler = (e) => {
-  //   if(e.keyCode === 13 && e.target.value) {
-  //     console.log(e.target.value)
-  //   }
-  // }
-
   render() {
     return (
       <div className="chat__input-wrapper">
-        <input type="text" className="chat__input" onKeyDown={this.props.onEnter} placeholder="Your message ..." />
+        <input
+          type="text"
+          className="chat__input"
+          onKeyDown={this.props.onEnter}
+          placeholder="Your message ..."
+        />
       </div>
-    ) 
+    );
   }
 }
 
